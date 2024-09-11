b9fe425f2fc1345877a9c0b0b8adf122c8cf0928
Palese Mirco (E) <mirco.palese@mermecgroup.com>
2024-09-11 11:15:07 +0200

    moved libraries from package.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component'; // Root component
-import { MyLibComponent } from 'my-lib';
 
 @NgModule({
     declarations: [
@@ -12,7 +11,6 @@ import { MyLibComponent } from 'my-lib';
     imports: [
         // Import other Angular modules or custom modules her
 
-        MyLibComponent,
         BrowserModule,
     ],
     providers: [
